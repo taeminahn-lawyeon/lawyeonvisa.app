20f2286974cc431df3768b0f3b588b045c48aeaf
taeminahn-lawyeon <taemin.ahn@lawyeon.com>
2025-12-08 14:47:05 +0900

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -21,7 +21,7 @@ async function signInWithGoogle() {
         const { data, error } = await supabase.auth.signInWithOAuth({
             provider: 'google',
             options: {
-                redirectTo: window.location.origin + '/dashboard-jnu.html'
+                redirectTo: window.location.origin + '/index.html'
             }
         });
         
